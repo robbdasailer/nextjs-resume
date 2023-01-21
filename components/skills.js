b3c8d6148ba539def881eb6bda1697941f02cd8a
Jimmy Bourque <jimmybchopps@gmail.com>
2023-01-21 13:22:53 -0600

    Fixed skills blurb bug

--- components/skills.js
+++ components/skills.js
@@ -4,17 +4,20 @@ import Slide from '@mui/material/Slide';
 import InfoIcon from '@mui/icons-material/Info';
 import CallToActionIcon from '@mui/icons-material/CallToAction';
 import { Typography } from '@mui/material';
-import Popover from '@mui/material/Popover';
+import Popper from '@mui/material/Popper';
+import Fade from '@mui/material/Fade';
 import { useState } from 'react';
 
 const Skills = (props) => {
 	const skills = props.skills;
 
 	const [anchorEl, setAnchorEl] = useState(null);
+	const [open, setOpen] = useState(false);
 	const [blurb, setBlurb] = useState(null);
 
 	const handlePopoverOpen = (event, blurb) => {
 		setBlurb(blurb);
+		setOpen((previousOpen) => !previousOpen);
 		setAnchorEl(event.currentTarget);
 	};
 
@@ -23,8 +26,6 @@ const Skills = (props) => {
 		setAnchorEl(null);
 	};
 
-	const open = Boolean(anchorEl);
-
 	return (
 		<>
 			<Typography variant='h6' sx={{ ml: 1.75 }}>
@@ -32,36 +33,43 @@ const Skills = (props) => {
 			</Typography>
 			{skills.map((skill) => (
 				<Box key={skill.id} sx={{ width: '90%', m: 2 }}>
-					<Typography
-						aria-owns={open ? 'mouse-over-popover' : undefined}
-						aria-haspopup='true'
-						onMouseEnter={() => handlePopoverOpen(event, skill.blurb)}
-						onMouseLeave={handlePopoverClose}
-						component='span'
-					>
+					<Typography component='span'>
 						{skill.skillName}
-						<InfoIcon sx={{ ml: 1, height: '1rem', width: '1rem' }} />
+						<InfoIcon
+							sx={{ ml: 1, height: '1rem', width: '1rem' }}
+							aria-owns={open ? 'mouse-over-popover' : undefined}
+							aria-haspopup='true'
+							onClick={() => handlePopoverOpen(event, skill.blurb)}
+						/>
 					</Typography>
-					<Popover
-						id='mouse-over-popover'
-						sx={{
-							pointerEvents: 'none',
-						}}
+					<Popper
+						id={skill.id}
 						open={open}
 						anchorEl={anchorEl}
-						anchorOrigin={{
-							vertical: 'bottom',
-							horizontal: 'left',
-						}}
-						transformOrigin={{
-							vertical: 'top',
-							horizontal: 'left',
-						}}
-						onClose={handlePopoverClose}
-						disableRestoreFocus
+						placement='left'
+						modifiers={[
+							{
+								name: 'preventOverflow',
+								enabled: true,
+								options: {
+									altAxis: true,
+									altBoundary: true,
+									tether: true,
+									rootBoundary: 'viewport',
+									padding: 8,
+								},
+							},
+						]}
+						transition
 					>
-						<Typography sx={{ p: 1 }}>{blurb}</Typography>
-					</Popover>
+						{({ TransitionProps }) => (
+							<Fade {...TransitionProps} timeout={350}>
+								<Box sx={{ border: 1, p: 1, bgcolor: 'background.paper' }}>
+									<Typography sx={{ p: 1 }}>{blurb}</Typography>
+								</Box>
+							</Fade>
+						)}
+					</Popper>
 					<Slide direction='right' in={true}>
 						<LinearProgress variant='determinate' value={skill.rating} />
 					</Slide>
